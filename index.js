db56fb8f772d7746f8f7183a47f27addf35c5ea6
mendelr20 <95175747+mendelr20@users.noreply.github.com>
2022-02-06 19:01:59 -0500

    commit tree b9ed4ff6bbece2bab398812e7c4f6dd99ed9106b

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// code your solution here
+
+
+
+
+function superbowlWin(record){
+  const found = record.find(function (element){
+      return element.result === "W"
+  });
+  if (found){
+    return found.year
+  }
+  return found
+  
+} 
+
+
+
